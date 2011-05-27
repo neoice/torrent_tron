68124ba0fb7a46090682e4b72e87c41e885276c7
rk <neoice@neoice.net>
2011-05-27 04:06:27 -0700

    cleaned some commented junk out. 	modified:   daemon/daemon.py

--- daemon/daemon.py
+++ daemon/daemon.py
@@ -12,7 +12,6 @@ import xmlrpc2scgi as xs
 rtorrent_host='scgi://127.0.0.1:5000'
 rt = xs.RTorrentXMLRPCClient(rtorrent_host)
 
-
 ### start
 # TODO: switch to multi-call for performance
 for torrent_hash in rt.download_list(""):
@@ -25,16 +24,3 @@ for torrent_hash in rt.download_list(""):
     torrent.speed_down      = rt.d.get_down_rate(torrent_hash)
 
     torrent.save()
-
-
-#country, bool = Country.objects.get_or_create( id=c.find('id').text
-#
-#38         # fill
-#39         country.name = c.find('name').text
-#40         country.population = c.find('citizen-count').text
-#41  42         country.access_time = datetime.datetime.now()
-#43         country.save() # commit         
-
-
-
-# for each in rtc.download_list(''):
